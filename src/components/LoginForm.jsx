f599f9c68094b2b2e00e1de32375686a70f6ab69
erykov1 <erykov1@gmail.com>
2023-12-14 19:24:25 +0100

    requesty dla logowania i rejestracji

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,25 +1,48 @@
 import React, {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    axios({
+      method: 'post',
+      url: 'http://localhost:8080/api/auth/token',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        username: username,
+        password: password,
+      }
+    }).then((response) => {
+      localStorage.setItem('token', response.data)
+      navigate('/')
+    }).catch((error) =>{
+      console.log(error)
+    })
+  }
 
   return (
     <div className="login-register-form-container">
       <form>
         <div className="mb-3">
           <label htmlFor="loginInput" className="form-label">
-            Podaj login
+            Nazwa użytkownika
           </label>
-          <input type="email" className="form-control" id="loginInput" />
+          <input type="text" className="form-control" id="loginInput" onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputPassword1" className="form-label">
             Hasło
           </label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
+          <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setPassword(e.target.value)}/>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-primary" onClick={handleLogin}>
           Zaloguj się
         </button>
       </form>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,16 +1,44 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [repeatPassword, setRepeatPassword] = useState('')
+  const [showPopover, setShowPopover] = useState(false);
+  const navigate = useNavigate()
+
+  const handleRegister = (event) => {
+    event.preventDefault()
+    if (repeatPassword === password) {
+      event.preventDefault()
+      axios({
+        method: 'post',
+        url: 'http://localhost:8080/api/user/register',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: {
+          username: username,
+          password: password,
+        }
+      }).then((response) => {
+        navigate('/signin')
+      }).catch((error) =>{
+        console.log(error)
+      })
+    } else {
+      setShowPopover(true)
+    }
+  }
 
   return (
     <div className="login-register-form-container">
-      <form>
+      <form onSubmit={handleRegister}>
         <div class="mb-3">
           <label for="loginInput" class="form-label">Podaj login</label>
-          <input type="email" class="form-control" id="loginInput" onChange={(e) => setUsername(e.target.value)}/>
+          <input type="text" class="form-control" id="loginInput" onChange={(e) => setUsername(e.target.value)}/>
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Hasło</label>
@@ -22,6 +50,14 @@ const RegisterForm = () => {
         </div>
         <button type="submit" class="btn btn-primary">Załóż konto</button>
       </form>
+      {showPopover && (
+        <div className="popover-container">
+          <div className="popover-arrow"></div>
+          <div className="popover-content">
+            <p>Wpisane dane są niepoprawne</p>
+          </div>
+        </div>
+      )}
     </div>
   )
   
